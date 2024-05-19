1c2b99812bca0dbcb40f8aa764b6a3750b8c35d5
Dan Mindru <mindrudan@gmail.com>
2024-05-19 20:56:44 +0200

    Add Page UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Button } from '@/components/shared/ui/button';
+import { LandingPrimaryVideoCtaSection } from '@/components/landing/cta/LandingPrimaryCta';
+import { LandingProductHuntAward } from '@/components/landing/social-proof/LandingProductHuntAward';
+import { LandingSocialProof } from '@/components/landing/social-proof/LandingSocialProof';
+import { LandingDiscount } from '@/components/landing/discount/LandingDiscount';
 
 function App() {
-  const [count, setCount] = useState(0)
+ const avatarItems = [
+    {
+      imageSrc: 'https://picsum.photos/id/64/100/100',
+      name: 'John Doe',
+    },
+    {
+      imageSrc: 'https://picsum.photos/id/65/100/100',
+      name: 'Jane Doe',
+    },
+    {
+      imageSrc: 'https://picsum.photos/id/669/100/100',
+      name: 'Alice Doe',
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <LandingPrimaryVideoCtaSection
+      title="Transform Your Business Today"
+      description="Say goodbye to inefficiencies and hello to success with our groundbreaking AI app. Streamline your workflow, boost productivity, and maximize revenue effortlessly."
+      autoPlay
+      controls={false}
+      videoSrc="https://cache.shipixen.com/features/11-pricing-page-builder.mp4"
+      withBackground
+      withBackgroundGlow
+      variant="secondary"
+      backgroundGlowVariant="secondary"
+      leadingComponent={<LandingProductHuntAward />}
+    >
+      <Button size="xl" variant="secondary" asChild>
+        <a href="#">Buy Now</a>
+      </Button>
+
+      <Button size="xl" variant="outlineSecondary">
+        <a href="#">Learn More</a>
+      </Button>
+
+      <LandingDiscount
+        discountValueText="$350 off"
+        discountDescriptionText="for the first 10 customers (2 left)"
+      />
+
+      <LandingSocialProof
+        className="w-full mt-12"
+        showRating
+        numberOfUsers={99}
+        suffixText="happy users"
+        avatarItems={avatarItems}
+      />
+    </LandingPrimaryVideoCtaSection>
+  );
 }
 
 export default App
